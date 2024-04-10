9fe1c3142565a43a47d8597ea8a522c2a5d3efa4
harshach <harshachennoor@gmail.com>
2024-04-10 15:25:08 -0500

    dbPropertyChange

--- obj/Debug/net8.0/ngwmapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ngwmapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ngwmapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869c982cea3459daf4136812063ec7374c8b4cf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+040467c03e9c9d1c855ffa83a93f1a35e551bdcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ngwmapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ngwmapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
